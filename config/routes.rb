717fe3b930929c549d49cd2e0198b0d9ededf0d8
David Lewis <davidlewisp@gmail.com>
2018-04-24 13:06:37 -0700

    Add authentication and factory bot

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'sessions#new'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+end
---
+++ spec/factories.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory(:task) do
+    name('Knit the sock monkey')
+  end
+end
